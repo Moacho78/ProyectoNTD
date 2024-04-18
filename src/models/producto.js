b5c66a7e57c74c893a39cd6f3cbf0f4fe73c9718
Moacho78 <160978496+Moacho78@users.noreply.github.com>
2024-04-18 11:12:17 -0500

    Add files via upload

---
+++ src/models/producto.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+const productoSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+    },
+    precio: {
+        type: Number,
+        required: true,
+    },
+    categoria: {
+        type: String,
+        required: true,
+    },
+    descripcion: {
+        type: String,
+        required: true,
+    }
+});
+module.exports = mongoose.model("producto", productoSchemaSchema);
