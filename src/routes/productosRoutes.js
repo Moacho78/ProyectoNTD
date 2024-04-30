cff56d2f5d762ea822b0f67e20721047a938b887
Derly <herrerayuyis97@gmail.com>
2024-04-30 10:46:23 -0500

    Modificaciones
    Se elimino el archivo producto de models, y se modificó el archivo productosRoutes eliminando unos endpoints

--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -1,16 +1,9 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
-const productoSchema = require("../models/producto");
-//Nuevo producto
-router.post("/productos", (req, res) => {
-    const producto = productoSchema(req.body);
-    producto
-        .save() //guardar
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }));
-});
+const productoSchema = require("../models/inventario");
+
 module.exports = router;
-//CONSULTAR TODOS LOS ANIMALES
+//CONSULTAR TODOS LOS PRODUCTOS
 router.get("/productos", (req, res) => {
     productoSchema.find()
         .then((data) => res.json(data))
@@ -25,31 +18,3 @@ router.get("/productos/:nombre", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
-
-//Modificar un producto por su id 
-router.put("/productos/:id", (req, res) => {
-    const { id } = req.params;
-    const { nombre, edad, tipo, fecha } = req.body;
-    productoSchema
-        .updateOne({ _id: id }, {
-            $set: { nombre, edad, tipo, fecha }
-        })
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }));
-});
-
-//Eliminar un animal por su id
-
-router.delete("/productos/:id", (req, res) => {
-    const { id } = req.params;
-    productoSchema
-        .findByIdAndDelete(id)
-        .then((data) => {
-            res.json(data);
-        })
-        .catch((error) => {
-            res.json({ message: error });
-        });
-});
-
-
