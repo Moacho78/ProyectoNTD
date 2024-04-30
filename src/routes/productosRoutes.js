d8200f004c8da4eee0afcfe0dc7a6012bd18bb3c
Derly <herrerayuyis97@gmail.com>
2024-04-30 11:02:17 -0500

    inventarioGestor
    Se añadió los endpoints al inventario del administrador

--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
-const productoSchema = require("../models/inventario");
+const inventarioSchema = require("../models/inventario");
 
 module.exports = router;
 //CONSULTAR TODOS LOS PRODUCTOS
 router.get("/productos", (req, res) => {
-    productoSchema.find()
+    inventarioSchema.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
@@ -13,7 +13,7 @@ router.get("/productos", (req, res) => {
 //Consultar un producto por su nombre
 router.get("/productos/:nombre", (req, res) => {
     const { nombre } = req.params;
-    productoSchema
+    inventarioSchema
         .findById(nombre)
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
