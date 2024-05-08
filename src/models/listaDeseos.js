05f99eec0b613864435970ec9e0ff188f9b38d53
Miguel <miguel.angel746@hotmail.com>
2024-05-08 16:55:57 -0500

    Modificación el esquema lista deseos
    Se modificaron los campos de producto y usuario para realizar la respectiva instancia entre las colecciones y adicionalmente se elimino el documento de rutas producto que no seria necesaria

--- src/models/listaDeseos.js
+++ src/models/listaDeseos.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose"); // importando el componente mogoose
 const listaDeseo = mongoose.Schema({
-    producto: {
-        type: any,
-        required: true,
-    },
-    user: {
-        type: String,
-        required: true,
-    }
+    producto: [{type:mongoose.Schema.Types.ObjectId,ref: 'inventario' , require:true}],
+    user: [{type:mongoose.Schema.Types.ObjectId,ref: 'user' , require:true}]
    
 });
 module.exports = mongoose.model("listaDeseo", listaDeseoSchemaSchema);
